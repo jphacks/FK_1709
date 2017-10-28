1bfac6f84984e9845e93bc6579e4383a1db20b23
Tomoki ICHIKAWA <ichikawa1060@gmail.com>
2017-10-28 14:28:58 +0900

    Add Columns To User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, 
+         :omniauthable, omniauth_providers: [:facebook]
+
+  validates :name, presence: true
+  validates :sex, presence: true
+  validates :birthday, presence: true
+
+  enum sex: { male: 0, female: 1 }
+
+  enum married_status: { unanswered_ms: 0, unmarried: 1,　divorce: 2, widowed: 3 }
+              
 end
