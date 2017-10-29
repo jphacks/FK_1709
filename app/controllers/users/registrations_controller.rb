8e93e3ba3afd2091eded068912fcf828a6ade51c
Tomoki ICHIKAWA <ichikawa1060@gmail.com>
2017-10-29 14:02:02 +0900

    Implement Profile of user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -71,12 +71,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :birthday,:marred_status, :sibling, :job, :income, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :sex, :birthday,:marred_status, :sibling, :job, :income, :image, :post_data, :profile])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :sex, :birthday, :marred_status, :sibling, :job, :income, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :sex, :birthday, :marred_status, :sibling, :job, :income, :image, :post_data, :profile])
   end
 
   # The path used after sign up.
