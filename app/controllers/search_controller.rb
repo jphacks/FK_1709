3f5b56cf7b9d79c7b17bd00a4b9687701f8f055f
Kansei Kogami <k_kansei_k@yahoo.co.jp>
2017-10-29 11:41:29 +0900

    add image at profile & doing push like

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,22 @@ class SearchController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    if params[:like].to_i == 1
+      like = Like.new(from_id: @myid, to_id: @user.id)
+      like.save
+    end
+
+    @myid = current_user.id
+
+    @like = Like.find_by(from_id: @myid, to_id: @user.id)
+
+    if @like == nil
+      @image_path = 'like/LikeButton.svg'
+    else
+      @image_path = 'like/LikedButton.svg'
+    end
+
   end
 
   private
