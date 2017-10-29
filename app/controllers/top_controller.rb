2f361eb520e600aeb7a165c5a828526a45fdb174
Tomoki ICHIKAWA <ichikawa1060@gmail.com>
2017-10-29 13:44:58 +0900

    Remove wasteful code

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,9 +1,4 @@
 class TopController < ApplicationController
   def index
-    Thread.start do
-      a = `cd #{Rails.root}/python; python predict_with_svm.py ./test/itaya.txt` 
-      p "aaa"
-      p a 
-    end
   end
 end
